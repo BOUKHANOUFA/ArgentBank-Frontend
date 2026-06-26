ce117e5ee8261d40cfc40bec910e820fc232fd48
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-06-26 16:40:22 +0200

    Intégration des pages en React et mise en place du routing+Transformation du HTML en composants React (Home, Login)+Configuration du routage avec React Router dans App.jsx+Ajout de BrowserRouter dans main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 function App() {
-  return <h1>ArgentBank</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
 export default App;
