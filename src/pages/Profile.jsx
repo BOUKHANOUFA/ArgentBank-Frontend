2f41ef70755f5fda86899b56d7d58b152da8c522
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-06-28 12:21:08 +0200

    modification de le page profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,89 +1,41 @@
 import "../main.css";
+import { Navigate, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
+
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+
+  //  condition
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
   return (
     <>
       <nav className="main-nav">
-        <a className="main-nav-logo" href="/">
+        <Link className="main-nav-logo" to="/">
           <img
             className="main-nav-logo-image"
             src="/src/assets/img/argentBankLogo.png"
             alt="Argent Bank Logo"
           />
-          <h1 className="sr-only">Argent Bank</h1>
-        </a>
+        </Link>
 
         <div>
-          <a className="main-nav-item" href="/profile">
-            <i className="fa fa-user-circle"></i>
+          <Link className="main-nav-item" to="/profile">
             Tony
-          </a>
+          </Link>
 
-          <a className="main-nav-item" href="/">
-            <i className="fa fa-sign-out"></i>
+          <Link className="main-nav-item" to="/">
             Sign Out
-          </a>
+          </Link>
         </div>
       </nav>
 
       <main className="main bg-dark">
-        <div className="header">
-          <h1>
-            Welcome back
-            <br />
-            Tony Jarvis!
-          </h1>
-          <button className="edit-button">Edit Name</button>
-        </div>
-
-        <h2 className="sr-only">Accounts</h2>
-
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-            <p className="account-amount">$2,082.79</p>
-            <p className="account-amount-description">Available Balance</p>
-          </div>
-
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">
-              View transactions
-            </button>
-          </div>
-        </section>
-
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-            <p className="account-amount">$10,928.42</p>
-            <p className="account-amount-description">Available Balance</p>
-          </div>
-
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">
-              View transactions
-            </button>
-          </div>
-        </section>
-
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-            <p className="account-amount">$184.30</p>
-            <p className="account-amount-description">Current Balance</p>
-          </div>
-
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">
-              View transactions
-            </button>
-          </div>
-        </section>
+        <h1>Welcome back Tony</h1>
       </main>
-
-      <footer className="footer">
-        <p className="footer-text">Copyright 2020 Argent Bank</p>
-      </footer>
     </>
   );
 }
