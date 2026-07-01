2435fcb0d7378ac7adce896f812be496b70a68ca
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-07-01 10:38:52 +0200

    CRÉER LA PAGE TRANSACTIONS

---
+++ src/pages/Transaction.jsx
@@ -0,0 +1,32 @@
+import Navbar from "../components/Navbar";
+import "../main.css";
+
+export default function Transactions() {
+  return (
+    <>
+      <Navbar />
+
+      <main className="main bg-dark">
+        <h1 style={{ color: "white", textAlign: "center" }}>
+          Transactions
+        </h1>
+
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3>Transaction #1</h3>
+            <p>$50.00</p>
+            <p>Grocery</p>
+          </div>
+        </section>
+
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3>Transaction #2</h3>
+            <p>$120.00</p>
+            <p>Shopping</p>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
