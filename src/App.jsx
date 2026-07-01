d342884ab8dfc19f0ce00e45e79c0693c49f020c
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-07-01 10:36:47 +0200

    ajouter le route de la page tansactions dans app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+import Transactions from "./pages/Transactions"; 
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/transactions" element={<Transactions />} />
     </Routes>
   );
 }
