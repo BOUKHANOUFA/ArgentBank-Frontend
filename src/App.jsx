6dd5867ad69333d44c8694f2d24f3713ad414489
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-07-04 11:08:28 +0200

    GARDER LE USER APRÈS REFRESH

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,38 @@ import Profile from "./pages/Profile";
 import Transactions from "./pages/Transactions"; 
 import Footer from "./components/Footer";
 
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "./redux/userSlice";
+
 
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      fetch("http://localhost:3001/api/v1/user/profile", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch(
+            loginSuccess({
+              token: token,
+              user: data.body,
+            })
+          );
+        })
+        .catch((err) => console.log(err));
+    }
+  }, []);
+
   return (
     <>
     <Routes>
